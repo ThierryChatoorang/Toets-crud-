3ddff72c83dd920adfd582871bf25c1f673df7ca
ThierryChatoorang <9027345@student.zadkine.nl>
2025-04-08 13:09:29 +0200

    deel 2
    de code in de tweede fase

---
+++ toets crud/config.php
@@ -0,0 +1,12 @@
+<?php
+// auteur: Vul hier je naam in
+// functie: configuratiebestand
+
+define("DATABASE", "reizen");
+define("SERVERNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+
+define("CRUD_TABLE", "klant");
+
+?>
---
+++ toets crud/insert.php
@@ -0,0 +1,51 @@
+<?php
+    // functie: formulier en database insert fiets
+    // auteur: Vul hier je naam in
+
+    echo "<h1>Insert Fiets</h1>";
+
+    require_once('functions.php');
+	 
+    // Test of er op de insert-knop is gedrukt 
+    if(isset($_POST) && isset($_POST['btn_ins'])){
+
+        // test of insert gelukt is
+        if(insertRecord($_POST) == true){
+            echo "<script>alert('Fiets is toegevoegd')</script>";
+        } else {
+            echo '<script>alert("Fiets is NIET toegevoegd")</script>';
+        }
+    }
+?>
+<html>
+    <body>
+        <form method="post">
+
+        <label for="idklant">idklant:</label>
+        <input type="text" id="idklant" name="idklant" required><br>
+
+        <label for="naam">naam:</label>
+        <input type="text" id="naam" name="naam" required><br>
+
+        <label for="adres">adres:</label>
+        <input type="text" id="adres" name="adres" required><br>
+
+        <label for="postcode">postcode:</label>
+        <input type="number" id="postcode" name="postcode" required><br>
+
+        <label for="plaats">plaats:</label>
+        <input type="text" id="plaats" name="plaats" required><br>
+
+        <label for="telefoon">telefoon:</label>
+        <input type="number" id="telefoon" name="telefoon" required><br>
+
+        <label for="email">email:</label>
+        <input type="text" id="email" name="email" required><br>
+
+        <input type="submit" name="btn_ins" value="Insert">
+        </form>
+        
+        <br><br>
+        <a href='index.php'>Home</a>
+    </body>
+</html>
